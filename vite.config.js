3ec8569873481f38d1c991103311a6a2f2e1b348
Mohammad Darvishi <mamad@Mohammads-MacBook-Pro.local>
2026-02-18 22:28:34 +0000

    Update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ command }) => {
-  const base = process.env.GITHUB_PAGES === 'true' ? '/telepathy/' : '/'
+  const base = process.env.GITHUB_PAGES === 'true' ? '/telepaty/' : '/'
 
   return {
     base,
